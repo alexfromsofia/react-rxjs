8e2591a56034834d13c85875ef0a47f9d9415a1b
Alexander Georgiev <aleksandar_georgiev@starsgroup.com>
2022-08-23 17:11:49 +0300

    Initial setup

---
+++ src/store.tsx
@@ -0,0 +1,35 @@
+import { BehaviorSubject, map } from "rxjs";
+
+export interface Pokemon {
+  id: number;
+  name: string;
+  type: string[];
+  hp: number;
+  attack: number;
+  defense: number;
+  special_attack: number;
+  special_defense: number;
+  speed: number;
+  power?: number;
+}
+
+export const rawPokemon$ = new BehaviorSubject<Pokemon[]>([]);
+
+export const pokemonWithPower$ = rawPokemon$.pipe(
+  map((pokemon) =>
+    pokemon.map((p) => ({
+      ...p,
+      power:
+        p.hp +
+        p.attack +
+        p.defense +
+        p.special_attack +
+        p.special_defense +
+        p.speed,
+    }))
+  )
+);
+
+fetch("/pokemon-simplified.json")
+  .then((res) => res.json())
+  .then((data) => rawPokemon$.next(data));
